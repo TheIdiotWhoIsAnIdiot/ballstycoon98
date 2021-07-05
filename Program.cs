511017155897fe1fe2f1917ef3993825fdda79a3
TheIdiotWhoIsAnIdiot <43867838+TheIdiotWhoIsAnIdiot@users.noreply.github.com>
2021-07-05 07:45:01 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BALLS98
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int balls = 0;
+            Console.WriteLine("Welcome to Balls98, the latest and greatest technology!");
+            Console.WriteLine("Type \"p\" to get a ball!");
+            string input = Console.ReadLine();
+            while (true)
+            {
+                if (input == "p")
+                {
+                    balls++;
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Command! You absolute buffoon!");
+                }
+                string answer = Console.ReadLine();
+                
+                if (answer == "p")
+                {
+                    Console.WriteLine($"+ 1 Ball! you now have {balls} balls!");
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Command! You absolute buffoon!");
+                    balls--;
+                    continue;
+                }
+               }
+            }
+        }
+    }
